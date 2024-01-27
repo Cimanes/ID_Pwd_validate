6b8752b839d2340f36623e2086c4b7777b7eeb4c
Cimanes <cimanes75@telefonica.net>
2024-01-27 18:07:11 +0100

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,130 +1,90 @@
 // Define the msg element and the input password element (node to be able to use .value)
-const
-  idTip = document.getElementById("idMessage"),
-  idIn = document.getElementById("idNum"),
-  pwdTip = document.getElementById("pwdMessage"),
-  pwdIn = document.getElementById("pwd");
+const idTip = document.getElementById("idMessage"),
+	idIn = document.getElementById("idNum"),
+	pwdTip = document.getElementById("pwdMessage"),
+	pwdIn = document.getElementById("pwd");
 
-  // Validate ID input field
-idIn.onkeyup =  function() { 
-  if (this.checkValidity()) {
-    this.classList.remove("invalid");
-    this.classList.add("valid");
-  } 
-  else {
-    this.classList.remove("valid");
-    this.classList.add("invalid");
-  }
-}
+// Validate ID input field
+idIn.onkeyup = function () {
+	if (this.checkValidity()) {
+		this.classList.remove("invalid");
+		this.classList.add("valid");
+	} else {
+		this.classList.remove("valid");
+		this.classList.add("invalid");
+	}
+};
 
 // Click on the password field => show the message box:
-idIn.onfocus = function() { idTip.style.display = "block"; }
+pwdIn.onfocus = function () {
+	pwdTip.style.display = "block";
+};
 
 // Click outside of the password field => hide the message box:
-idIn.onblur = function() { idTip.style.display = "none"; }
-
-// Click on the password field => show the message box:
-pwdIn.onfocus = function() { pwdTip.style.display = "block"; }
-
-// Click outside of the password field => hide the message box:
-pwdIn.onblur = function() { pwdTip.style.display = "none"; }
+pwdIn.onblur = function () {
+	pwdTip.style.display = "none";
+};
 
 // Type inside the password field => check requirements:
-pwdIn.onkeyup = function() {
-  // Validate lowercase letters
-  const 
-    lower = document.getElementById("lower"),
-    lowerReq = /[a-z]/g;
-  if(pwdIn.value.match(lowerReq)) {  
-    lower.classList.remove("invalid");
-    lower.classList.add("valid");
-  } else {
-    lower.classList.remove("valid");
-    lower.classList.add("invalid");
-  }
+pwdIn.onkeyup = function () {
+	// Validate lowercase letters
+	const lower = document.getElementById("lower"),
+		lowerReq = /[a-z]/g;
+	if (pwdIn.value.match(lowerReq)) 		lower.classList.replace('invalid', 'valid');
+	else lower.classList.replace('valid', 'invalid');
 
-  // Validate capital letters
-  const 
-    upper = document.getElementById("upper"),
-    upperReq = /[A-Z]/g;
-  if(pwdIn.value.match(upperReq)) {  
-    upper.classList.remove("invalid");
-    upper.classList.add("valid");
-  } else {
-    upper.classList.remove("valid");
-    upper.classList.add("invalid");
-  }
+	// Validate capital letters
+	const upper = document.getElementById("upper"),
+		upperReq = /[A-Z]/g;
+	if (pwdIn.value.match(upperReq)) 		upper.classList.replace('invalid', 'valid');
+	else upper.classList.replace('valid', 'invalid');
 
-  // Validate numberReq
-  const 
-    number = document.getElementById("number"),
-    numberReq = /[0-9]/g;
-  if(pwdIn.value.match(numberReq)) {  
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-   } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
-  }
+	// Validate numberReq
+	const number = document.getElementById("number"),
+		numberReq = /[0-9]/g;
+	if (pwdIn.value.match(numberReq))		number.classList.replace('invalid', 'valid');
+	else number.classList.replace('valid', 'invalid');
 
-  // Validate symbols
-  const
-    symbol = document.getElementById("symbol") 
-    symbolReq = /[!@#$%^&*_=+\-]/g;
-  if(pwdIn.value.match(symbolReq)) {  
-    symbol.classList.remove("invalid");
-    symbol.classList.add("valid");
-  } else {
-    symbol.classList.remove("valid");
-    symbol.classList.add("invalid");
-  }
-    
-  // Validate length
-  const length = document.getElementById("length")
-  if(pwdIn.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
-  } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
-  }
+	// Validate symbols
+	const symbol = document.getElementById("symbol"),
+		symbolReq = /[!@#$%^&*_=+\-]/g;
+	if (pwdIn.value.match(symbolReq)) 	symbol.classList.replace('invalid', 'valid');
+	else symbol.classList.replace('valid', 'invalid');
 
-  if (pwdIn.checkValidity()) {
-    pwdIn.classList.remove("invalid");
-    pwdIn.classList.add("valid");
-  } 
-  else {
-    pwdIn.classList.remove("valid");
-    pwdIn.classList.add("invalid");
-  }
+	// Validate length
+	const length = document.getElementById("length");
+	if (pwdIn.value.length >= 8) 				length.classList.replace('invalid', 'valid');
+  else length.classList.replace('valid', 'invalid');
 
-  // Measure quality of the password
-  const 
-    arrQuality = [
-      pwdIn.value.match(lowerReq), 
-      pwdIn.value.match(upperReq), 
-      pwdIn.value.match(numberReq), 
-      pwdIn.value.match(symbolReq), 
-      (pwdIn.value.length >= 8)
-    ],
-    quality = arrQuality.filter(Boolean).length / arrQuality.length;
+	// Global validation
+	if (pwdIn.checkValidity()) 					pwdIn.classList.replace('invalid', 'valid');
+	else pwdIn.classList.replace('valid', 'invalid');
 
-  // Update quality mark 
-  if (pwdIn.value.length > 0) {
-    pwdQuality.innerHTML = arrQuality.filter(Boolean).length + " / " + arrQuality.length;  
-  }
-  else pwdQuality.innerHTML = '';
-	
+	// Measure quality of the password
+	const arrQuality = [
+			pwdIn.value.match(lowerReq),
+			pwdIn.value.match(upperReq),
+			pwdIn.value.match(numberReq),
+			pwdIn.value.match(symbolReq),
+			pwdIn.value.length >= 8
+		],
+		quality = arrQuality.filter(Boolean).length / arrQuality.length;
+
+	// Update quality mark
+	if (pwdIn.value.length > 0) {
+		pwdQuality.innerHTML =
+			arrQuality.filter(Boolean).length + " / " + arrQuality.length;
+	} else pwdQuality.innerHTML = "";
+
 	// Update colored bars
 	for (let i = 1; i <= arrQuality.length; i++) {
-    const bar = document.getElementById("pwd" + i);
-    if (quality >= i/5) {
-      bar.classList.remove("hide");
-      bar.classList.add("show");
-    } 
-    else {
-      bar.classList.remove("show");
-      bar.classList.add("hide");
-    }
-  }
-}
+		const bar = document.getElementById("pwd" + i);
+		if (quality >= i / 5) {
+			bar.classList.remove("hide");
+			bar.classList.add("show");
+		} else {
+			bar.classList.remove("show");
+			bar.classList.add("hide");
+		}
+	}
+};
